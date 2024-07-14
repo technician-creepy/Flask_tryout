b0d8e4a1a9936ca77596c43d49fbe6c93e45ace4
Patrick-Kox <technician-creepy@proton.me>
2024-07-14 16:04:32 +0200

    basic Flask code

--- flask_app.py
+++ flask_app.py
@@ -4,17 +4,6 @@ app=Flask(__name__)
 def home():
     return "<p>Dit is het huiswerk van Patrick Kox voor de NHA opleiding: 'programmeren voor Beginners'</p>"
 
-'''
-De reactie van de terminal op get commando 'git checkout -b main is':
-"Switched to a new branch 'main'
-
-Gevolg van ingeven van commando: 'git checkout -b main'
-
-git maakt een nieuwe 'tak' aan van de hoofd repository en schakeld om naar deze tak
-hiermee kunnen we werken aan deze tak terwijl we altijk nog kunnen terugschakelen naar
-de orginele tak, of deze later met elkaar samenvoegen.
-'''
-
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
 
